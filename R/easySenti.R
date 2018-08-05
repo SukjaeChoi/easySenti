9c4f07b3d17319e20f8d3143f1cd3d5d8bbbe722
SukjaeChoi <lingua@daum.net>
2018-08-05 21:25:16 +0900

    Add files via upload

--- R/easySenti.R
+++ R/easySenti.R
@@ -11,27 +11,27 @@
 #' @return result vector.
 #' @export
 #' @examples
-#' cntSenti(docs = docs, positive=positive, negative=negative, t=0, sigmoid=FALSE, t.s=0.3)
+#' cntSenti(docs = docs, positive=positive, negative=negative, t=0, sigmoid=FALSE, t.s=0.3, t.s2=0.1)
 
 easySenti <- function(docs, positive, negative, t=0, sigmoid=FALSE, t.s=0.3, t.s2=0.1)
 {
   library(RHINO)
-  initRhino()   
-  
+  initRhino()
+
   POS <- lapply(docs, getMorph, "ALL")  	        # 형태소 분석
-  
-  POS.vec <- unlist(POS)                        #벡터 타입으로 변환 
+
+  POS.vec <- unlist(POS)                        #벡터 타입으로 변환
   pos.matches.num <- match(POS.vec, positive)   #긍정어 벡터 번호
   neg.matches.num <- match(POS.vec, negative)  	#부정어 벡터 번호
-  
+
   pos.matches <- !is.na(pos.matches.num)        #긍정어는 TRUE, 그 외는 FALSE
   neg.matches <- !is.na(neg.matches.num)        #부정어는 TRUE, 그 외는 FALSE
-  
+
   pos.sum <- sum(pos.matches)	                  #긍정어 검사결과에서 TRUE 개수 카운트
   neg.sum <- sum(neg.matches) 	                #부정어 검사결과에서 TRUE 개수 카운트
-  
+
   result <- NULL
-  
+
   if(sigmoid==FALSE){                            # sigmoid = FALSE 인 경우
     #감정값 계산
     result.sum <- pos.sum - neg.sum
@@ -67,8 +67,8 @@ easySenti <- function(docs, positive, negative, t=0, sigmoid=FALSE, t.s=0.3, t.s
       result <- "NEUTRAL"
     }
   }
-  
-  
+
+
   return(result)
 }
 
